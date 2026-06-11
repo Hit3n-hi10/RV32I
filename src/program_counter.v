2a45282e9d054b06226de548e474c530e3a620ac
Anshika@2205 <anshika.singh24b@iiitg.ac.in>
2026-06-11 10:57:53 +0530

    Add program_counter module for PC functionality

---
+++ src/program_counter.v
@@ -0,0 +1,21 @@
+//RTL CODE
+`timescale 1ns / 1ps
+module program_counter (
+ input wire clk,
+ input wire rst,
+ input wire [31:0] next_pc,
+ output reg [31:0] pc
+);
+
+ always @(posedge clk or posedge rst) begin
+  if (rst) 
+  begin
+     pc <= 32'h0000_0000;
+  end 
+   else
+ begin
+     pc <= next_pc;
+  end
+ end
+
+endmodule
