4785df860930053e8a9edbf37606373094120bbf
Anshika@2205 <anshika.singh24b@iiitg.ac.in>
2026-07-05 11:26:26 +0530

    Refactor program_counter to use active-low reset

--- src/program_counter.v
+++ src/program_counter.v
@@ -1,21 +1,19 @@
 //RTL CODE
-`timescale 1ns / 1ps
 module program_counter (
- input wire clk,
- input wire rst,
- input wire [31:0] next_pc,
- output reg [31:0] pc
+    input  wire        clk,
+    input  wire        rst_n,    
+    input  wire [31:0] next_pc,
+    output reg  [31:0] pc
 );
 
- always @(posedge clk or posedge rst) begin
-  if (rst) 
-  begin
-     pc <= 32'h0000_0000;
-  end 
-   else
- begin
-     pc <= next_pc;
-  end
- end
-
+    always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin        
+      pc <= 32'h0000_0000;
+    end
+     else
+      begin
+            pc <= next_pc;
+     end
+    end
 endmodule
+
