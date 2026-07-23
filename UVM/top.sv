7567f0eb3b6f20a41b17fe9678e12e52a53226f3
Hiten Kumar <hitenkr302@gmail.com>
2026-07-23 15:04:22 +0530

    Update top.sv

--- UVM/top.sv
+++ UVM/top.sv
@@ -1,24 +1,51 @@
 
 `timescale 1ns/1ns
 
-`inlcude "uvm_macros.svh" 
+`include "uvm_macros.svh" 
 import uvm_pkg::*;
+
+`include "interface.sv"
+`include "rv32i_mon_item.sv"
+`include "seq_item.sv"
+`include "sequence.sv"
+`include "sequencer.sv"
+`include "driver.sv"
+`include "monitor.sv"
+`include "agent.sv"
+`include "predictor.sv"
+`include "scoreboard.sv"
+`include "environment.sv"
+`include "test.sv"
 
 module top();
   
   logic clk;
   logic rst;
   
+   rv32i_intf intf(.clk(clk));
+  
   rv32i_top dut(.clk(intf.clk),.rst(intf.rst));
   
-  rv32i_intf intf(.clk(clk),.rst(rst));
-  
+ initial begin
+   uvm_config_db#(virtual rv32i_intf)::set(null,"*","vif",intf);
+ end
+  initial begin
   clk=0;
+   // rst = 1;
+   // #20;
+   // rst=0;
+  end
   always #10 clk = ~clk;
   
   initial begin
     $monitor("$time","Clk = %d",clk);
-  	$100 finish;
+    //$dumpfile("dump.vcd"); 
+    //$dumpvars;
+    //#100 $finish;
+  end
+  
+  initial begin
+    run_test("rv32i_test");
   end
   
 endmodule
