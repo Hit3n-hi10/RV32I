74de1a9bf4c09ede739be4e29ffec0bffabd8b53
Hiten Kumar <hitenkr302@gmail.com>
2026-07-23 15:03:00 +0530

    Update seq_item.sv

--- UVM/seq_item.sv
+++ UVM/seq_item.sv
@@ -1,11 +1,17 @@
+
 class rv32i_seq_item extends uvm_sequence_item;
 
-  `uvm_object_utils(rv32i_seq_item)
+  `uvm_object_utils(rv32i_seq_item) 
+ 
+  
+  bit [31:0] instr[$];
+  
+  
   
   //standard constructor
-  function new(string name = "rv32i_seq_item",uvm_component parent);
+  function new(string name = "rv32i_seq_item");
   
-    super.new(name,parent);
+    super.new(name);
     `uvm_info("seq_item class","Constructor",UVM_MEDIUM)
     
   endfunction
